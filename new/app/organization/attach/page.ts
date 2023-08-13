af893404c693087beaf1cb220e26851a931ef47d
Victor Porton <porton.victor@gmail.com>
2023-08-13 23:01:57 +0300

    misc

---
+++ new/app/organization/attach/page.ts
@@ -0,0 +1,25 @@
+import { ethAddressToBuffer } from "@/util/eth";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const j = JSON.parse(await req.json());
+    const data: {
+        name: string,
+        colonyNickName: string,
+        colonyAddress: Buffer,
+        tokenAddress: Buffer,
+        tokenAuthorityAddress: Buffer,
+      } = {
+        name: j.name,
+        colonyNickName: j.colonyNickName,
+        colonyAddress: ethAddressToBuffer(j.colonyAddress),
+        tokenAddress: ethAddressToBuffer(j.tokenAddress),
+        tokenAuthorityAddress: ethAddressToBuffer(j.tokenAuthorityAddress),
+      };
+    const userEmail = ''; // FIXME
+    const prisma = new PrismaClient();
+    let org = await prisma.organization.create({data});
+    await prisma.organizationsUsers.create({data: {userEmail, organizationId: org.id}});
+    return NextResponse.json({});
+}
--- new/app/organization/join/page.ts
+++ new/app/organization/join/page.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const j = JSON.parse(await req.json());
-    const userEmail = j.userEmail;
+    const userEmail = ''; // FIXME
     const orgId = j.organizationId;
     const prisma = new PrismaClient();
     await prisma.organizationsUsers.create({data: {userEmail: userEmail, organizationId: orgId}});
