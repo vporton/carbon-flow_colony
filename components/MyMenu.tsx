1b19c0910be543ba9f704d7680cc2cf8bfad48af
Victor Porton <porton.victor@gmail.com>
2023-08-14 23:36:27 +0300

    better menu formatting

--- components/MyMenu.tsx
+++ components/MyMenu.tsx
@@ -5,8 +5,8 @@ export default function MyMenu(props: {}) {
     return (
         <List style={{ display: 'flex', flexDirection: 'row', padding: 0 }}>
             {/* TODO: Clicking outside item does not open the link. */}
-            <ListItem><Link href="/login" className="nav-link">Login</Link></ListItem>
-            <ListItem><Link href="/" className="nav-link">Organizations</Link></ListItem>
+            <ListItem style={{width: 'auto'}}><Link href="/login" className="nav-link">Login</Link></ListItem>
+            <ListItem style={{width: 'auto'}}><Link href="/" className="nav-link">Organizations</Link></ListItem>
         </List>
     );
 }
