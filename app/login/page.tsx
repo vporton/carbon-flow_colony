c01e20141f1a5123c29c407d84e0149a121630d1
Victor Porton <porton.victor@gmail.com>
2023-08-14 16:03:05 +0300

    simplified

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,16 +3,10 @@
 import { signIn, useSession } from 'next-auth/react';
 
 const LoginPage = () => {
-  const handleLogin = () => {
-    signIn('google'); // Replace 'google' with your chosen provider
-  };
-
-  // let session = useSession();
-
   return (
     <div>
       <h1>Login</h1>
-      <button onClick={handleLogin}>Sign in with Google</button>
+      <button onClick={() => signIn('google')}>Sign in with Google</button>
     </div>
   );
 };
