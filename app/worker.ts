86c1013c88f7608a6c6484cb911cf1ef6ba10c9b
Victor Porton <porton.victor@gmail.com>
2023-11-28 21:28:23 +0200

    misc

--- app/worker.ts
+++ app/worker.ts
@@ -2,34 +2,72 @@ import { colonyNetwork, ethProvider } from "@/util/serverSideEthConnect";
 import { ColonyEventManager } from "@colony/sdk";
 import { PrismaClient } from "@prisma/client/extension";
 import { IColonyEvents__factory as ColonyEventsFactory } from '@colony/events/types';
+import { utils } from "ethers";
+import { TransactionKind } from "@/util/transactionKinds";
+import { ethAddressToBuffer } from "@/util/eth";
 
 async function worker() {
     const prisma = new PrismaClient();
-    const txs = await prisma.transaction.findMany(
-        {select: {id: true, tx: true, kind: true, blockChecked: true}, where: {confirm: false}}
-    );
     
     const manager = new ColonyEventManager(ethProvider);
+    // TODO: Remove the below commented out code.
     // const colonyAddedEventSource = eventManager.createEventSource(ColonyAdded);
     // const colonyAddedFilter = eventManager.createFilter(colonyAddedEventSource, "ColonyAdded");
     // Here is an example: https://github.com/JoinColony/colonyJS/blob/main/packages/sdk/examples/browser/src/events.ts
     // See lines 35 and 36 in the above example
     const colonyEventSource = manager.createEventSource(ColonyEventsFactory);
 
-    // const domainEvents = manager.createMultiFilter(
+    // const ourEvents = manager.createMultiFilter(
     //     colonyEventSource,
-    //     ['DomainAdded(address,uint256)', 'DomainMetadata(address,uint256,string)'],
-    //     colonyAddress,
-    //   );
-    
-    // TODO
-    // manager.provider.on('block', async (no) => {
-    //     manager.
-    // });
+    //     ['ColonyAdded(uint256,address,address)'], // FIXME
+    //     // colonyAddress,
+    // );
 
-    for (const transaction in txs) {
-        // colonyNetwork.
+    // Ethers provider
+    const provider = colonyNetwork.getInternalNetworkContract().provider; // That should work, but it is not elegant.
+
+    async function processEvent(event: any, id: number, kind: TransactionKind) {
+        switch (kind) {
+            case TransactionKind.CREATE_ORGANIZATION:
+                const {name, colonyNickName} = prisma.createNewOrganizationTransaction.findFirstOrThrow({
+                    select: {tokenName: true, tokenSymbol: true, colonyNickName: true, name: true},
+                    where: {transaction: id},
+                })
+                prisma.organization.create({
+                    name,
+                    colonyNickName,
+                    colonyAddress: ethAddressToBuffer(event.args.colonyAddress),
+                    token: ethAddressToBuffer(event.args.tokenAddress),
+                    tokenAuthorityAddress: ethAddressToBuffer("0x0"), // FIXME                  
+                });
+                break;
+        }
     }
+
+    // TODO: Remove old events.
+
+    // TODO: Process remaining events on startup.
+    // const txs = await prisma.transaction.findMany(
+    //     {select: {id: true, tx: true, kind: true, blockChecked: true}, where: {confirm: false}}
+    // );
+    
+    const filter = {
+        // address: THE_ADDRESS_OF_YOUR_CONTRACT,
+        // topics: [
+        //     utils.id('ColonyAdded(uint256,address,address)')
+        // ],
+    };
+    provider.on(filter, async (_log, event) => { // FIXME: Is `async` supported here?
+        const transactionHash = event.transactionHash; // TODO: correct?
+        const { id, kind } = await prisma.transaction.findFirstOrThrow(
+            // TODO: Are all `select` args necessary?
+            {select: {id: true, kind: true, blockChecked: true}, where: {confirm: false, tx: transactionHash}} // FIXME
+        );
+        await processEvent(event, id, kind);
+        // TODO: Is `event.block` a correct field?
+        // TODO: Should use the function `NOW` instead of `new Date()`.
+        await prisma.transaction.update({where: {id}, data: {confirmed: true, blockChecked: event.block, lastCheckedAt: new Date()}});
+    });
 }
 
 worker().then(() => {});
