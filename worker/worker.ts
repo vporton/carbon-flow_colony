5923ad270ec0373041492419c020727f7f63ac1e
Victor Porton <porton.victor@gmail.com>
2023-12-23 10:23:28 +0200

    working on worker

--- worker/worker.ts
+++ worker/worker.ts
@@ -6,6 +6,22 @@ import { utils } from "ethers";
 import { TransactionKind } from "@/../util/transactionKinds";
 import { ethAddressToBuffer } from "@/../util/eth";
 
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
 async function worker() {
     const prisma = new PrismaClient();
     
