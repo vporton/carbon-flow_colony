520899e28051c4be4dfbf6e423bcdd939bf493c9
Victor Porton <porton.victor@gmail.com>
2024-01-26 15:34:06 +0200

    misc

--- main/app/api/organization/create/route.ts
+++ main/app/api/organization/create/route.ts
@@ -6,37 +6,47 @@ import { txsDisplay } from "@/app/api/worker-callback/route";
 import EthExecuting from "@/components/EthExecuting";
 import { PrismaClient } from "@prisma/client";
 import { assert } from "console";
+import { useSession } from "next-auth/react";
 import { NextResponse } from "next/server";
 import { useContext } from "react";
 
-export async function POST(req: Request) {
-    const j = await req.json();
-    const {
-        tokenName, tokenSymbol, colonyNickName, organizationName,
-    }: {
-        tokenName: string, tokenSymbol: string, colonyNickName: string, organizationName: string,
-    } = j;
+export function POST(req: Request) {
+    const session = useSession();
+    const email = session.data?.user?.email;
+    if (email === undefined) {
+        return;
+    }
 
-    const tx = await colonyNetwork.createColony({ name: tokenName, symbol: tokenSymbol }, colonyNickName); // TODO: More parameters
-    const txHash = await getTransactionHash(await tx.tx().encode());
-    txsDisplay.onSubmitted(txHash, "create colony");
-    console.log("txHash", txHash);
+    async function doIt() {
+        const j = await req.json();
+        const {
+            tokenName, tokenSymbol, colonyNickName, organizationName,
+        }: {
+            tokenName: string, tokenSymbol: string, colonyNickName: string, organizationName: string,
+        } = j;
+    
+        const tx = await colonyNetwork.createColony({ name: tokenName, symbol: tokenSymbol }, colonyNickName); // TODO: More parameters
+        const txHash = await getTransactionHash(await tx.tx().encode());
+        txsDisplay[email!].onSubmitted(txHash, "create colony");
+        console.log("txHash", txHash);
 
-    const prisma = new PrismaClient();
-    // TODO: database transaction
-    const dbTrans = await prisma.transaction.create({data: {
-        tx: ethHashToBuffer(txHash),
-        kind: TransactionKind.CREATE_ORGANIZATION,
-        confirmed: false,
-    }});
-    await prisma.createNewOrganizationTransaction.create({data: {
-        id: dbTrans.id,
-        tokenName,
-        tokenSymbol,
-        colonyNickName,
-        organizationName,
-    }});
-    const [tx2, _promise] = await tx.metaTx().send();
+        const prisma = new PrismaClient();
+        // TODO: database transaction
+        const dbTrans = await prisma.transaction.create({data: {
+            tx: ethHashToBuffer(txHash),
+            kind: TransactionKind.CREATE_ORGANIZATION,
+            confirmed: false,
+        }});
+        await prisma.createNewOrganizationTransaction.create({data: {
+            id: dbTrans.id,
+            tokenName,
+            tokenSymbol,
+            colonyNickName,
+            organizationName,
+        }});
+        const [tx2, _promise] = await tx.metaTx().send();
+    }
+    doIt().then(() => {});
     // console.assert(tx2.hash === txHash, "Programming error: hashes don't match: %s !== %s", txHash, tx2.hash); // This fails!
 
     // // TODO: (should be `await` before `waitForCreateOrganizationConfirmed`?)
--- main/app/api/worker-callback/route.ts
+++ main/app/api/worker-callback/route.ts
@@ -12,6 +12,10 @@ class StoreTxsServer {
 
     onSubmitted(txHash: string, message: string) {
         for (const ws of this.websockets) {
+            ws.onclose = () => {
+                this.websockets = Array.from(this.websockets.filter(e => e !== ws)); // TODO: inefficient
+                // TODO: Remove `this` itself, when elements are drained.
+            };
             ws.send(JSON.stringify({tx: txHash, message, state: 'submitted'}));
         }
     }
@@ -24,14 +28,15 @@ class StoreTxsServer {
 }
 
 // FIXME: Make it per-connection (or per-user?)
-export const txsDisplay: StoreTxsServer = new StoreTxsServer();
+export const txsDisplay: { [user: string]: StoreTxsServer } = {}; //new StoreTxsServer();
 
+// TODO: Pass `userId` param.
 export async function POST(req: Request, res: Response) {
     if (req.headers.get('Authorization') !== process.env.BACKEND_SECRET!) {
         return NextResponse.json({ error: "Allowed only by backend" }, { status: 403 });
     }
     // Forward to client using WebSocket.
-    const j: {tx: string} = await req.json();
-    txsDisplay.onMined(j.tx);
+    const j: {tx: string, userId: number} = await req.json();
+    txsDisplay[j.userId].onMined(j.tx);
     return NextResponse.json({});
 }
