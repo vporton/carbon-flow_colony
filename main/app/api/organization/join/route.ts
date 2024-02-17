210603cee07c9a967fcd2a6346152e2ac1c79481
Victor Porton <porton.victor@gmail.com>
2024-02-17 20:17:26 +0200

    corrected routes

--- main/app/api/organization/join/route.ts
+++ main/app/api/organization/join/route.ts
@@ -1,27 +1,20 @@
-import { ethHashToBuffer, getTransactionHash } from "@/../util/eth";
-import { colonyNetwork } from "@/../util/serverSideEthConnect";
-import { TransactionKind } from "@/../util/transactionKinds";
-import { EthTxsContext } from "@/app/_sublayout2";
-import { txsDisplay } from '@/../../util/workerClient';
-import EthExecuting from "@/components/EthExecuting";
 import { PrismaClient } from "@prisma/client";
-import { assert } from "console";
-import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
 import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
 
 export async function POST(req: Request) {
     const session = await getServerSession();
-    const email = session?.user?.email;
-    if (email === undefined) {
-        return;
-    }
-
-    const j = await req.json();
-    const organizationId = j.organizationId;
+    const userEmail = session!.user!.email as string;
+    const j = await req.json()
+    const organizationId = j.organizationId as number;
 
     const prisma = new PrismaClient();
-    prisma.organizationsUsers.create({ data: {organizationId, userEmail: email!} });
+    await prisma.organizationsUsers.create({
+        data: {
+            userEmail,
+            organizationId,
+        },
+    });
 
     return NextResponse.json({});
 }
