8f8fd761b212892ae39e83808614bb4895cd7bd6
Victor Porton <porton.victor@gmail.com>
2024-02-17 18:21:13 +0200

    bug fix

--- main/next.config.js
+++ main/next.config.js
@@ -1,6 +1,14 @@
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 require('dotenv').config({ path: '../.env' })
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+          config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },    
+}
 
 module.exports = nextConfig
