19134e7510ddec773473e04f06820572a64d4285
Victor Porton <porton.victor@gmail.com>
2024-02-19 00:49:34 +0200

    better source formatting

--- main/components/OrganizationInside.tsx
+++ main/components/OrganizationInside.tsx
@@ -16,12 +16,14 @@ export default function OrganizationInside(props: {
         <p>List of organization&apos;s carbon tokens (usually, should consist of one element):</p>
         {props.tokens.length ?
             <ul>
-                {props.tokens.map(t => <li key={t.id}>Token {t.id}/{t.id+1} {t.comment !== undefined ? `(${encode(t.comment)})` : ""}
-                    {" "}<Link href={`/token/tax/${t.id}`}>{t.tax * 100}% tax</Link> {/* TODO: set tax */}
-                    {" "}<Link href={`/mint/${t.id}`}>mint</Link>,
-                    <Link href={`/conversions/${t.id}`}>conversions</Link>,
-                    <Button>propose to remove</Button> {/* TODO: onClick */}
-                    </li>)}
+                {props.tokens.map(t =>
+                    <li key={t.id}>Token {t.id}/{t.id+1} {t.comment !== undefined ? `(${encode(t.comment)})` : ""}
+                        {" "}<Link href={`/token/tax/${t.id}`}>{t.tax * 100}% tax</Link> {/* TODO: set tax */}
+                        {" "}<Link href={`/mint/${t.id}`}>mint</Link>,
+                        <Link href={`/conversions/${t.id}`}>conversions</Link>,
+                        <Button>propose to remove</Button> {/* TODO: onClick */}
+                    </li>
+                )}
             </ul> : <p>(none)</p>}
         <p>Propose: <Button>Create new token</Button>, <Button>Add existing token (advanced)</Button></p>
     </>
