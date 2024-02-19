3ac718ae1724fb0cd47d06bd5055775896001f44
Victor Porton <porton.victor@gmail.com>
2024-02-19 03:59:26 +0200

    removed a space in the code

--- util/data.ts
+++ util/data.ts
@@ -1,3 +1,3 @@
 import CarbonInfo from "@porton/carbon-flow/artifacts/Carbon.deployed.json";
 
-export const carbonTokenAddress: string = (CarbonInfo as {[chain: string] : any})[process.env.CHAIN_ID!].address;
+export const carbonTokenAddress: string = (CarbonInfo as {[chain: string]: any})[process.env.CHAIN_ID!].address;
