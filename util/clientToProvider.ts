117b4b61951d1f47f275e3b343c054e5fc488c79
Victor Porton <porton.victor@gmail.com>
2024-02-19 17:37:16 +0200

    bug fix

--- util/clientToProvider.ts
+++ util/clientToProvider.ts
@@ -1,4 +1,4 @@
-import { FallbackProvider, JsonRpcProvider } from 'ethers'
+import { providers } from 'ethers'
 import { useMemo } from 'react'
 import type { Chain, Client, Transport } from 'viem'
 import { type Config, useWalletClient } from 'wagmi'
@@ -12,12 +12,12 @@ export function clientToProvider(client: Client<Transport, Chain>) {
   }
   if (transport.type === 'fallback') {
     const providers = (transport.transports as ReturnType<Transport>[]).map(
-      ({ value }) => new JsonRpcProvider(value?.url, network),
+      ({ value }) => new providers.JsonRpcProvider(value?.url, network),
     )
     if (providers.length === 1) return providers[0]
-    return new FallbackProvider(providers)
+    return new providers.FallbackProvider(providers)
   }
-  return new JsonRpcProvider(transport.url, network)
+  return new providers.JsonRpcProvider(transport.url, network)
 }
 
 /** Action to convert a viem Client to an ethers.js Provider. */
